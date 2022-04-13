45f93df75da89576373c750d09979ff023be3c89
Jose Malcher Jr <contato@josemalcher.net>
2022-04-13 16:31:32 -0300

    Interface Gráfica com Vue JS #01

---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="pills">
+  <Pill v-for="word in groupdWords" :key="word.name"
+  :name="word.name" :amount="word.amount"
+  />
+</div>
+</template>
+
+<script>
+import Pill from "./Pill";
+export default {
+  name: "HomeInit",
+  components: {
+    Pill
+  },
+  data: () => ({
+    groupdWords: [
+      {name: 'i', amount:123},
+      {name: 'you', amount:1223},
+      {name: 'he', amount:333},
+    ]
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ most-used-words/src/components/Pill.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <span>{{name}}</span>
+  <span>{{amount}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: "PillHome",
+  props:['name', 'amount']
+}
+</script>
+
+<style scoped>
+
+</style>
