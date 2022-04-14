70e62c7272363fe5455b6a56dd98b51c998fba5f
Jose Malcher Jr <contato@josemalcher.net>
2022-04-14 16:11:58 -0300

    Interface Gráfica com Vue JS #02

--- most-used-words/src/components/Home.vue
+++ most-used-words/src/components/Home.vue
@@ -1,13 +1,16 @@
 <template>
-<div class="pills">
-  <Pill v-for="word in groupdWords" :key="word.name"
-  :name="word.name" :amount="word.amount"
-  />
-</div>
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupdWords" :key="word.name"
+            :name="word.name" :amount="word.amount"
+      />
+    </div>
+  </v-container>
 </template>
 
 <script>
 import Pill from "./Pill";
+
 export default {
   name: "HomeInit",
   components: {
@@ -15,14 +18,37 @@ export default {
   },
   data: () => ({
     groupdWords: [
-      {name: 'i', amount:123},
-      {name: 'you', amount:1223},
-      {name: 'he', amount:333},
+      {name: 'i', amount: 123},
+      {name: 'you', amount: 1223},
+      {name: 'he', amount: 333},
+      {name: 'i', amount: 123},
+      {name: 'you', amount: 1223},
+      {name: 'he', amount: 333},
+      {name: 'i', amount: 123},
+      {name: 'you', amount: 1223},
+      {name: 'he', amount: 333},
+      {name: 'i', amount: 123},
+      {name: 'you', amount: 1223},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
+      {name: 'he', amount: 333},
     ]
   }),
 }
 </script>
 
 <style scoped>
-
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
